4b7f307880a4b69d9358a6c58364283b7e67c4e5
Vazgen <vazgen04@gmail.com>
2021-11-03 23:15:01 +0400

    PracticeHomework

--- src/Homeworks/PracticeHomework/PracticeHomework.java
+++ src/Homeworks/PracticeHomework/PracticeHomework.java
@@ -4,13 +4,13 @@ public class PracticeHomework {
 
 
     //փոխակերպել տրված րոպեն վայրկյանի ու վերադարձնել
-    long convert(int minutes, long secunds) {
-        return minutes * secunds;
+    long convert(int minutes) {
+        return minutes * 60;
     }
 
     //ընդունել տարին, վերադարձնել օրերի քանակը։
-    int calcAge(int years, int days) {
-        return years * days;
+    int calcAge(int years) {
+        return years * 365;
     }
 
     //վերադարձնել տրված թվի հաջորդ թիվը
@@ -19,25 +19,31 @@ public class PracticeHomework {
     }
 
     //վերադարձնել true եթե տրված a և b թվերը իրար հավասար են, եթե ոչ՝ false
+
     boolean isSameNum(int a, int b) {
-        return false;
+        if (a == b) {
+            return true;
+        } else {
+            return false;
+        }
     }
+        //վերադարձնել true-եթե թիվը փոքր կամ հավասար է 0-ին։
+        //մնացած դեպքերում վերադարձնել false
+        boolean lessThanOrEqualToZero(int number){
+            return number <= 0;
+        }
 
-    //վերադարձնել true-եթե թիվը փոքր կամ հավասար է 0-ին։
-    //մնացած դեպքերում վերադարձնել false
-    boolean lessThanOrEqualToZero(int number) {
-        return false;
+        //վերադարձնել ընդունող բուլյանի ժխտված արժեքը։
+        boolean reverseBool(boolean value) {
+            return !value;
+        }
+
+        //վերադարձնել երկու մասիվի ամենամեծ length-ը
+
+        int maxLength ( int[] array1, int[] array2){
+            return array1.length > array2.length ?
+                    array1.length : array2.length;
+
+        }
+
     }
-
-    //վերադարձնել ընդունող բուլյանի ժխտված արժեքը։
-    boolean reverseBool(boolean value) {
-        return false;
-    }
-
-    //վերադարձնել երկու մասիվի ամենամեծ length-ը
-    int maxLength(int[] array1, int[] array2) {
-        return 0;
-
-    }
-
-}
--- src/Homeworks/PracticeHomework/PracticeHomeworkTest.java
+++ src/Homeworks/PracticeHomework/PracticeHomeworkTest.java
@@ -4,51 +4,16 @@ public class PracticeHomeworkTest {
 
     public static void main(String[] args) {
 
-
         PracticeHomework ph = new PracticeHomework();
 
-        //փոխակերպել տրված րոպեն վայրկյանի ու վերադարձնել
-
-        int minutes = 1;
-        long secunds = 60;
-        long result = ph.convert(minutes, secunds);
-        System.out.println(result);
-
-        //ընդունել տարին, վերադարձնել օրերի քանակը։
-
-        result = ph.calcAge(2, 365);
-        System.out.println(result);
-
-        //վերադարձնել տրված թվի հաջորդ թիվը
-
-        result = ph.nextNumber(14);
-        System.out.println(result);
-
-        //վերադարձնել true եթե տրված a և b թվերը իրար հավասար են, եթե ոչ՝ false
-
-        boolean result1 = ph.isSameNum(10, 5);
-        System.out.println(result1);
-
-        //վերադարձնել true-եթե թիվը փոքր կամ հավասար է 0-ին։
-        //մնացած դեպքերում վերադարձնել false
-
-        int number = 1;
-        boolean result3 = ph.lessThanOrEqualToZero(number);
-        if (number <= 0) {
-            result3 = true;
-        } else result3 = false;
-        {
-            System.out.println(result3);
-        }
-
-        //վերադարձնել ընդունող բուլյանի ժխտված արժեքը։
-
-
-
-        //վերադարձնել երկու մասիվի ամենամեծ length-ը
-        
-
-
-
+        System.out.println(ph.convert(5));
+        System.out.println(ph.calcAge(2));
+        System.out.println(ph.nextNumber(187));
+        System.out.println(ph.isSameNum(4, 5));
+        System.out.println(ph.lessThanOrEqualToZero(0));
+        System.out.println(ph.reverseBool(true));
+        int[] array1 = {1};
+        int[] array2 = {1, 5, 88, 7};
+        System.out.println(ph.maxLength(array1, array2));
     }
 }
