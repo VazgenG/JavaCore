3c3f6a4a813d001a4f763abf2b491ce258d2461c
Vazgen <vazgen04@gmail.com>
2021-11-06 00:24:02 +0400

    PracticeHomework

---
+++ src/Homeworks/dynamicArray/DynamicArray.java
@@ -0,0 +1,49 @@
+package Homeworks.dynamicArray;
+
+public class DynamicArray {
+
+
+    // սա մեր հիմնական մասիվն է, որտեղ պահելու ենք ավելացվող էլեմենտները
+    private int[] array = new int[10];
+    //սա մեր մասիվի մեջ ավելացված էլեմենտների քանակն է
+    private int size = 0;
+
+    //ստուգել եթե մասիվի մեջ տեղ չկա, կանչել -> extend() և ավելացնենք
+    public void add(int value) {
+        if (value == array.length) {
+        } else extend();
+    }
+
+    //1․ստեղծել հին մասիվից 10 էլեմենտ ավելի մեծ մասիվ
+    //2․քցել հին մասիվի էլեմենտները նորի մեջ
+    //3․հին մասիվի հղումը կապել նոր մասիվի հղման հետ։
+
+    private void extend() {
+        int[] arrayNew = new int[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+                arrayNew[i] = array[i];
+                size++;
+
+        }
+    }
+
+
+    // եթե տրված ինդեքսը մեր ունեցած սահմաններում է, վերադարձնել
+    //մասիվի index-երորդ էլեմենտը։ Հակառակ դեպքում վերադարձնել -1։
+    public int getByIndex(int index) {
+        if (array[index] <= array.length) {
+            return index;
+        } else {
+            return -1;
+        }
+
+    }
+
+
+    //տպել մասիվի ավելացված էլեմենտները
+    public void print(int[] arrayNew) {
+        for (int size :arrayNew);
+       System.out.println(size);
+    }
+
+}
---
+++ src/Homeworks/dynamicArray/DynamicArrayTest.java
@@ -0,0 +1,13 @@
+package Homeworks.dynamicArray;
+
+public class DynamicArrayTest {
+    public static void main(String[] args) {
+
+        DynamicArray da = new DynamicArray();
+        da.add(10);
+
+      //  da.print(arrayNew[size]);
+
+
+    }
+}
